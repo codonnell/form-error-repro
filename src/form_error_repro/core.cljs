79383f77b541545d30c605f34a761ee99f7534e0
Chris O'Donnell <chris.bowdoin@gmail.com>
2020-01-15 21:42:32 -0500

    Default to broken version and use local fulcro.

--- src/form_error_repro/core.cljs
+++ src/form_error_repro/core.cljs
@@ -29,20 +29,20 @@
    :form-fields #{:user/name}}
   (ui-form {:onSubmit #(js/console.log "Submit")}
     ;; This version works
-    (ui-form-input {:onChange (fn [evt]
-                                (m/set-string! this :user/name :event evt)
-                                (comp/transact! this [(fs/mark-complete! {:field :user/name})]))
-                    :error (and (fs/invalid-spec? user :user/name) "User name cannot be blank")
-                    :fluid true
-                    :value name})
+    ;; (ui-form-input {:onChange (fn [evt]
+    ;;                             (m/set-string! this :user/name :event evt)
+    ;;                             (comp/transact! this [(fs/mark-complete! {:field :user/name})]))
+    ;;                 :error (and (fs/invalid-spec? user :user/name) "User name cannot be blank")
+    ;;                 :fluid true
+    ;;                 :value name})
     ;; This version does not work
-    ;; (ui-form-input (cond-> {:onChange (fn [evt]
-    ;;                                     (m/set-string! this :user/name :event evt)
-    ;;                                     (comp/transact! this [(fs/mark-complete! {:field :user/name})]))
-    ;;                         :fluid true
-    ;;                         :value name}
-    ;;                  (fs/invalid-spec? user :user/name)
-    ;;                  (assoc :error "User name cannot be blank")))
+    (ui-form-input (cond-> {:onChange (fn [evt]
+                                        (m/set-string! this :user/name :event evt)
+                                        (comp/transact! this [(fs/mark-complete! {:field :user/name})]))
+                            :fluid true
+                            :value name}
+                     (fs/invalid-spec? user :user/name)
+                     (assoc :error "User name cannot be blank")))
     (ui-button {:type "submit"} "Submit")))
 
 (def ui-my-form (comp/factory MyForm))
